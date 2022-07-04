de6d18675283943982dc5f287df8127a51c864cb
mayanjain <mayanjain17@gmail.com>
2022-07-04 14:24:20 +0530

    initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDcWMA6fDXFYMbJ1C5Bclgryh2e_71nXx8",
+  authDomain: "crypto-tracker-cf7ea.firebaseapp.com",
+  projectId: "crypto-tracker-cf7ea",
+  storageBucket: "crypto-tracker-cf7ea.appspot.com",
+  messagingSenderId: "173256859677",
+  appId: "1:173256859677:web:0e3d253ce77a00816ce6f4"
+};
+
+
+export default firebaseConfig;
